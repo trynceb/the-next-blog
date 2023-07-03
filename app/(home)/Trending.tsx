d7471ea0e1e3d95583b50edb7f1d3254f61cd8e8
Trynce Brandt <tjbrandt2013@gmail.com>
2023-07-03 13:53:08 -0400

    finish TrendingCard

--- app/(home)/Trending.tsx
+++ app/(home)/Trending.tsx
@@ -1,6 +1,32 @@
+import Link from "next/link"
 import React from "react";
 
-type Props = {}
+type TrendingCardProps = {
+    className?: string
+}
+
+const TrendingCard = ({ className }: TrendingCardProps) => {
+    return (
+        <Link
+            className={`${className}0 sm:mt-0 sm:h-auto relative mt-7 block w-full h-96 hover:opacity-70`}
+            // href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
+            href="/"
+        >
+            <div className="z-0 relative w-full h-full bg-wh-500">
+                image
+            </div>
+            <div className="absolute z-1 top-0 left-0 w-full h-full bg-gradient-gradual" />
+            <div className="absolute z-2 bottom-0 left-0 p-3">
+                <h4 className="inline-block px-5 py-1 font-semibold bg-accent-orange text-wh-900">
+                    category
+                </h4>
+                <div className="text-wh-100 mt-2">
+                    post title
+                </div>
+            </div>
+        </Link>
+    )
+}
 
 const Trending = (props: Props) => {
     return(
@@ -16,12 +42,15 @@ const Trending = (props: Props) => {
 
             {/* grid option */}
             <div className="sm:grid gap-5 grid-cols-4 grid-rows-2 sm:h-[600px] my-3">
-                <div className="col-span-2 row-span-2 basis-1/2 bg-wh-500"></div>
-                <div className="col-span-2 row-span-1 basis-1/2 bg-wh-500"></div>
-                <div className="col-span-1 row-span-1 basis-1/2 bg-wh-500"></div>
-                <div className="col-span-1 row-span-1 basis-1/2 bg-wh-500"></div>
+                <TrendingCard className="col-span-2 row-span-2 basis-1/2 bg-wh-500" />
+                <TrendingCard className="col-span-2 row-span-1 basis-1/2 bg-wh-500" />
+                <TrendingCard className="col-span-1 row-span-1 basis-1/2 bg-wh-500" />
+                <TrendingCard className="col-span-1 row-span-1 basis-1/2 bg-wh-500" />
             </div>
 
+            <p className="text-sm">
+                Maybe put a small brief description here.
+            </p>
         </section>
     )
 }
